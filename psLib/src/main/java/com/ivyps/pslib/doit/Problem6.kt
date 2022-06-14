e0c657d167776ae06bc3cd360653bea5fb5e73be
sykim <skim.ivy@gmail.com>
2022-06-14 15:09:44 +0900

    [studyingPS] 6번 문제 풀이 (백준 온라인 저지 2018 번) - @문제출처저서 : Do it! 알고리즘 코딩 테스트 | 김종관 저 | 이지스퍼블리싱 | 2022.04.05

---
+++ psLib/src/main/java/com/ivyps/pslib/doit/Problem6.kt
@@ -0,0 +1,39 @@
+package com.ivyps.pslib.doit
+
+import java.util.*
+
+/**
+ * @author sykim-ivy
+ * @since 2022-06-14
+ * @문제출처 : 백준 온라인 저지 2018 번
+ */
+class Problem6: Problem() {
+    override fun problemSolving() {
+        println("Hello PS / Problem6")
+        val sc = Scanner(System.`in`)
+        val N = sc.nextInt()
+
+        var result = 1
+        var sum = 1
+        var startP = 1
+        var endP = 1
+        while(endP != N) {
+            println("CASE ($startP, $endP)")
+            if(sum < N) {
+                endP++
+                sum += endP
+            } else if(sum > N) {
+                sum -= startP
+                startP++
+            } else {
+//                println("result CASE ($startP, $endP)")
+                result++
+                endP++
+                sum += endP
+            }
+        }
+
+        println("result = $result")
+
+    }
+}
