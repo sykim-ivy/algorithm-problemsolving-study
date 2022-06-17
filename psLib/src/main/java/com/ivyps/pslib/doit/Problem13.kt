b81656e44a66cbd8db3cb6f43c2037373ea012b6
sykim <skim.ivy@gmail.com>
2022-06-17 10:58:56 +0900

    [studyingPS] 13번 문제 풀이 (백준 온라인 저지 17298 번) : 큐 구현하기 - @문제출처저서 : Do it! 알고리즘 코딩 테스트 | 김종관 저 | 이지스퍼블리싱 | 2022.04.05

---
+++ psLib/src/main/java/com/ivyps/pslib/doit/Problem13.kt
@@ -0,0 +1,27 @@
+package com.ivyps.pslib.doit
+
+import java.util.*
+
+/**
+ * @author sykim-ivy
+ * @since 2022-06-17
+ * @문제출처 : 백준 온라인 저지 2750 번
+ */
+class Problem13: Problem() {
+    override fun problemSolving() {
+        println("Hello PS / Problem13")
+        val sc = Scanner(System.`in`)
+        val n = sc.nextInt()
+        val q: Queue<Int> = LinkedList()
+        for(i in 1..n) {
+            q.add(i)
+        }
+
+        while(q.size != 1) {
+            println("remove: ${q.poll()}")
+            println("addLast: ${q.add(q.poll())}")
+        }
+
+        println("result : ${q.poll()}")
+    }
+}
