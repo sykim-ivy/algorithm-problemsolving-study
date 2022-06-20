73c33efcba6580c2fa2b0215f9c8d67ec4258615
sykim <skim.ivy@gmail.com>
2022-06-20 10:19:49 +0900

    [studyingPS] 17번 문제 풀이 (백준 온라인 저지 1427 번) : 내림차순 정렬하기(삽입정렬 추가) - @문제출처저서 : Do it! 알고리즘 코딩 테스트 | 김종관 저 | 이지스퍼블리싱 | 2022.04.05

--- psLib/src/main/java/com/ivyps/pslib/doit/Problem17.kt
+++ psLib/src/main/java/com/ivyps/pslib/doit/Problem17.kt
@@ -6,6 +6,13 @@ import java.util.*
  * @author sykim-ivy
  * @since 2022-06-18
  * @문제출처 : 백준 온라인 저지 1427 번
+ *
+ * - 입력
+73425161
+ * - 출력
+Selection Sort : 76543211
+Bubble Sort : 76543211
+Insertion Sort : 76543211
  */
 class Problem17: Problem() {
     override fun problemSolving() {
@@ -18,9 +25,9 @@ class Problem17: Problem() {
             originArr = originArr.plus((i-'0'))
         }
 
-        // #1) 선택정렬
+        // #1) 선택정렬 : O(n*n)
         val a = originArr.clone()
-        val n = a.size
+        var n = a.size
         for(i in 0 until n) { // i는 정렬완료 후 삽입될 인덱스
             var max = i // 최소값들어갈 인덱스
             for(j in i until n) {
@@ -37,8 +44,9 @@ class Problem17: Problem() {
         }
         printArr("Selection Sort", a)
 
-        // #2) 버블 정렬
+        // #2) 버블 정렬 : O(n*n)
         val b = originArr.clone()
+        n = b.size
         for(i in 0 until n) {
             for(j in 1 until n-i) {
                 if(b[j] > b[j-1]) {
@@ -50,11 +58,35 @@ class Problem17: Problem() {
         }
         printArr("Bubble Sort", b)
 
+        // #3) 삽입 정렬 : O(n*n)
+        val c = originArr.clone()
+        val clist = c.toMutableList()
+        n = clist.size
+        for(i in 0 until n) {
+            var iIdx = i
+            for(j in 0..i){
+                if(clist[i] > clist[j]) {
+                    iIdx = j
+                    break
+                }
+            }
+            if(iIdx < i) {
+                clist.add(iIdx, clist.removeAt(i))
+            }
+        }
+        printArrList("Insertion Sort", clist)
+
 
         sc.close()
     }
 
     private fun printArr(title:String, a: IntArray) {
+        print("$title : ")
+        for(i in a) print("$i")
+        println()
+    }
+
+    private fun printArrList(title:String, a: MutableList<Int>) {
         print("$title : ")
         for(i in a) print("$i")
         println()
