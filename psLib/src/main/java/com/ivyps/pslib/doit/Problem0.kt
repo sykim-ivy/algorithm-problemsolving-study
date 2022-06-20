176ff65d1366487c0e39494f39a832489f9f7bad
sykim <skim.ivy@gmail.com>
2022-06-20 15:34:11 +0900

    [studyingPS] 템플릿 파일의 주석 문구 수정 - @문제출처저서 : Do it! 알고리즘 코딩 테스트 | 김종관 저 | 이지스퍼블리싱 | 2022.04.05

--- psLib/src/main/java/com/ivyps/pslib/doit/Problem0.kt
+++ psLib/src/main/java/com/ivyps/pslib/doit/Problem0.kt
@@ -4,6 +4,10 @@ package com.ivyps.pslib.doit
  * @author sykim-ivy
  * @since 2022-06-20
  * @문제출처 : 백준 온라인 저지 11004 번 (p.121)
+ * - 입력
+
+ * - 출력
+
  */
 class Problem0: Problem() {
     override fun problemSolving() {
